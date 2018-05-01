b0625090fcd6a25d557e1b2f559d4f770fd12be8
maxim spiridonov <spiridonov.maxim@simbirsoft.com>
2018-05-01 11:39:04 +0400

    added items for category, product

--- msuzi_opt/msuzi_opt/items.py
+++ msuzi_opt/msuzi_opt/items.py
@@ -8,7 +8,17 @@
 import scrapy
 
 
-class MsuziOptItem(scrapy.Item):
+class MsuziOptProductItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    props = scrapy.Field()
+    descr = scrapy.Field()
+    price = scrapy.Field()
+    referer = scrapy.Field()
+    name_images = scrapy.Field()
+
+
+class MsuziOptCatalogItem(scrapy.Item):
+    catalog = scrapy.Field()
+    url_catalog = scrapy.Field()
+
